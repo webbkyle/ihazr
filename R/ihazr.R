edcdbf682eec06dcea9105cefdf81d37483e3ed1
kylewebb <kylepwebb1992@gmail.com>
2018-08-15 09:28:41 -0400

    added description, parameters, return components of ihazr function and man folder

--- R/ihazr.R
+++ R/ihazr.R
@@ -1,19 +1,22 @@
 #' ihazr
 #'
 #' \code{ihazr} interactively updates hazard function for survival anaylisis.
-#' Allows the user to choose subsections of their data they wish to model.
+#' Allows the user to choose subsections of their data.
 #'
 #' @import htmlwidgets
-#'
+#' @param time A numerical vector of time data.
+#' @param status A binary vector representing event reached or not reached.
+#' @param marker A collection of numerical variables of interest for survival
+#'   analysis.
+#' @param width The width of the html widget. The default is NULL, which results
+#'   in intelligent automatic sizing based on the widget's container.
+#' @param height The height of the html widget. The default is NULL, which
+#'   results in intelligent automatic sizing based on the widget's container.
+#' @return The ihazr of \code{time} and \code{status} and \code{marker}
+#' @example ihazr(time, status, marker)
 #' @export
 ihazr <- function(time, status, marker, width = NULL, height = NULL) {
-
   # forward options using x
-#  x <- data.frame(
-#    time = time,
-#    status = status,
-#    marker = marker
-#  )
     x <- data.frame(
       time = time,
       status = status
