0ef0103cdf37fcb01341a6a4fbed3171123d6e04
kylewebb <kylepwebb1992@gmail.com>
2018-10-11 12:56:24 -0400

    removed R/<ihazr>.R file

--- R/<ihazr>.R
+++
@@ -1,7 +0,0 @@
-#' ihazr: A package that interactively updates the hazard function for survival analysis data.
-#'
-#' The ihazr package implements these important functions:
-#' ihazr
-#'
-#' @docType pakcage
-#' @name ihazr
